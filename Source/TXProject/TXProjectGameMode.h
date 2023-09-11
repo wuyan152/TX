05b33799acfa220a9e664d34798f5ff21428e539
WuYan <113519503+wuyan152@users.noreply.github.com>
2023-09-11 21:21:22 +0800

    Add files via upload

---
+++ Source/TXProject/TXProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TXProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATXProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATXProjectGameMode();
+};
+
+
+
